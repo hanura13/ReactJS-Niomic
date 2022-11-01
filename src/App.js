6269539a982d217238839ee662cfec8f6d71e0f6
hanura13 <hanuradiarjo@gmail.com>
2022-11-01 10:53:45 +0700

    Tugas 01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import MenuKontak from './Pages/MenuKontak'
+import MenuProduct from './Pages/MenuProduct'
+import MenuTentangKami from './Pages/MenuTentangKami'
+import MenuUtama from './Pages/MenuUtama'
+
+const Header =() => {
+    return(
+        <div>
+            <h1>Ini Halaman untuk Header</h1>
+        </div>
+    )
+}
+
+const Footer =() => {
+    return(
+        <div>
+            <h1>Ini Halaman untuk Footer</h1>
+        </div>
+    )
+}
 
 const App = () => {
   return (
-    <div>Hello React</div>
+    <div>
+        <Header/>
+        <MenuUtama/>
+        <MenuProduct/>
+        <MenuKontak/>
+        <MenuTentangKami/>
+        <Footer/>
+    </div>
   )
 }
 
