881b35e071ca9145710f26e6598ad9d201c79277
hanura13 <hanuradiarjo@gmail.com>
2022-11-01 12:08:52 +0700

    Add Components

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    var name = 'SUNDEV'
+    return (
+        <div>
+            <h4>Footer {name}</h4>
+        </div>
+    )
+}
+
+export default Footer
