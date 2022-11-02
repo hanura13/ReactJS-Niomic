a757aa878f0a55750b396fd5d3793350095d13e1
hanura13 <hanuradiarjo@gmail.com>
2022-11-02 12:33:21 +0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from 'react'
-import MenuKontak from './Pages/MenuKontak'
-import MenuProduct from './Pages/MenuProduct'
-import MenuTentangKami from './Pages/MenuTentangKami'
-import MenuUtama from './Pages/MenuUtama'
-
-const Header =() => {
-    return(
-        <div>
-            <h1>Ini Halaman untuk Header</h1>
-        </div>
-    )
-}
-
-const Footer =() => {
-    return(
-        <div>
-            <h1>Ini Halaman untuk Footer</h1>
-        </div>
-    )
-}
+import Footer from './Pages/Footer'
+import Header from './Pages/Header'
 
 const App = () => {
   return (
     <div>
         <Header/>
-        <MenuUtama/>
-        <MenuProduct/>
-        <MenuKontak/>
-        <MenuTentangKami/>
+
         <Footer/>
     </div>
   )
---
+++ src/Pages/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <h5>Tugas 2 ReactJS</h5>
+    </div>
+  )
+}
+
+export default Footer
